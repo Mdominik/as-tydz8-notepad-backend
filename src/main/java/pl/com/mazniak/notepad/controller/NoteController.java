ebd488287f3258e3978dd782bc082460e7f2165e
Mdominik <mazniakd@gmail.com>
2020-11-20 10:35:31 +0100

    CHanged to RestController

--- src/main/java/pl/com/mazniak/notepad/controller/NoteController.java
+++ src/main/java/pl/com/mazniak/notepad/controller/NoteController.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class NoteController {
     NoteRepo noteRepo;
